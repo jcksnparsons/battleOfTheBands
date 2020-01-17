484cfaa3135b9ccbf3b4e27715b01042ac94e9dc
Jack Parsons <jcksnparsons@yahoo.com>
2020-01-17 12:55:05 -0600

    removed unnecessary console.logs

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ const takeNumber = function (bandName) {
     bandNumber++;
 }
 
-const scum = takeNumber("Galactic Scum")
-console.log(scum)  // This should print "1. Galactic Scum" in the console
+const scum = takeNumber("Galactic Scum")// This should print "1. Galactic Scum" in the console
 
 const under = takeNumber("Underdogs")
-console.log(under)  // This should print "2. Underdogs" in the console
+
+const obj = takeNumber("Smart Objects")
