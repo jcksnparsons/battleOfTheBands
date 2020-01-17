31603c975233d1f91240619ee36ed839f5a37b7c
Jack Parsons <jcksnparsons@yahoo.com>
2020-01-17 13:05:00 -0600

    changed console.logs to return, much better code

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 let bandNumber = 1
 
 const takeNumber = function (bandName) {
-    console.log(bandNumber, bandName);
-    bandNumber++;
+    return `${bandNumber++}. ${bandName}`;
 }
 
-const scum = takeNumber("Galactic Scum")// This should print "1. Galactic Scum" in the console
+const scum = takeNumber("Galactic Scum")
+console.log(scum)
 
 const under = takeNumber("Underdogs")
-
-const obj = takeNumber("Smart Objects")
+console.log(under)
